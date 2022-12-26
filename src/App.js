a338e2ba1dac2c3d25ef9d6ba16134bc019f73e5
Santy <santiicarp17@gmail.com>
2022-12-26 12:02:29 -0300

    comment

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
+function App() {//djfgdfbhg
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          santy <code>src/App.js</code> and save to reload.
+          santy 
         </p>
         <a
           className="App-link"
